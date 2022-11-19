9b169337496362c6be1f6449c3ba7bb90552c8d0
oustn <kun.zeng.dev@gmail.com>
2022-11-19 11:26:55 +0800

    feat: empty new project

--- src/cli.ts
+++
@@ -1,6 +0,0 @@
-// Just to do
-function cli() {
-  console.log('todo');
-}
-
-export default cli;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,1 @@
-import test from 'ava';
-
-import { Mkcert } from '@/index';
-
-test('Mkcert Create success', async (t) => {
-  const mkcert = new Mkcert()
-  await mkcert.install('localhost')
-  t.pass()
-})
+console.log('a');
