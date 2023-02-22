87a507768475f882c94786b3e6873bf9ca7d10bd
oustn <kun.zeng.dev@gmail.com>
2023-02-22 09:58:16 +0800

    fix: update oss source name

--- src/mkcert/srouces/oss.ts
+++ src/mkcert/srouces/oss.ts
@@ -3,7 +3,7 @@ import {BaseSource, Release} from "./base";
 class OssSource extends BaseSource {
   getLatestRelease(target: string): Promise<Release | undefined> {
     return Promise.resolve({
-      name: 'mkcert',
+      name: `mkcert-v1.4.4-${target}`,
       version: '1.4.4',
       date: Date(),
       url: `https://pureui-static.oss-cn-hangzhou.aliyuncs.com/paas-component-custom/mkcerts/mkcert-v1.4.4-${target}`
